2aeb2aec2540518604b750d0ccc66e8e9e00c9a9
lcomplete <louchenabc@gmail.com>
2023-03-28 10:21:08 +0800

    fix: Configure SSLContext with a TrustManager that accepts any certificate

--- app/server/huntly-server/src/main/java/com/huntly/server/util/HttpUtils.java
+++ app/server/huntly-server/src/main/java/com/huntly/server/util/HttpUtils.java
@@ -4,9 +4,17 @@ import com.huntly.server.domain.model.ProxySetting;
 import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.StringUtils;
 
+import javax.net.ssl.KeyManager;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
 import java.net.InetSocketAddress;
 import java.net.ProxySelector;
 import java.net.http.HttpClient;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.time.Duration;
 
 /**
@@ -15,7 +23,17 @@ import java.time.Duration;
 @UtilityClass
 public class HttpUtils {
     public static HttpClient buildHttpClient(ProxySetting proxySetting, Integer timeoutSeconds) {
+        // Configure SSLContext with a TrustManager that accepts any certificate
+        SSLContext sslContext = null;
+        try {
+            sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(new KeyManager[0], new TrustManager[]{new DefaultTrustManager()}, null);
+        } catch (NoSuchAlgorithmException | KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
+        
         var clientBuilder = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(timeoutSeconds))
+                .sslContext(sslContext)
                 .followRedirects(HttpClient.Redirect.ALWAYS);
         if (proxySetting != null && StringUtils.isNotBlank(proxySetting.getHost())) {
             clientBuilder = clientBuilder.proxy(
@@ -25,6 +43,22 @@ public class HttpUtils {
             );
         }
         return clientBuilder.build();
+    }
+
+    private static class DefaultTrustManager implements X509TrustManager {
+
+        @Override
+        public void checkClientTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+        }
+
+        @Override
+        public void checkServerTrusted(X509Certificate[] arg0, String arg1) throws CertificateException {
+        }
+
+        @Override
+        public X509Certificate[] getAcceptedIssuers() {
+            return null;
+        }
     }
 
     /**
