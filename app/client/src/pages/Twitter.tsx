17571d29a6691697f3f9519955b171b34a326369
lcomplete <louchenabc@gmail.com>
2023-04-28 20:50:05 +0800

    feat: all page support date range filter

--- app/client/src/pages/Twitter.tsx
+++ app/client/src/pages/Twitter.tsx
@@ -3,6 +3,7 @@ import navLabels from "../components/Sidebar/NavLabels";
 import MainContainer from "../components/MainContainer";
 import React, {useState} from "react";
 import PageFilters, {PageFilterOptions} from "../components/PageFilters";
+import {getPageListFilter} from "../domain/utils";
 
 export default function Twitter() {
   const [pageFilterOptions, setPageFilterOptions] = useState<PageFilterOptions>({
@@ -30,7 +31,10 @@ export default function Twitter() {
 
   return <MainContainer>
     <PageList navLabel={navLabels.twitter}
-              filters={{contentType: 'TWEET', sort: pageFilterOptions.defaultSortValue, asc: pageFilterOptions.asc}}
+              filters={{
+                ...getPageListFilter(pageFilterOptions),
+                contentType: 'TWEET'
+              }}
               buttonOptions={{markRead: false}}
               filterComponent={<PageFilters options={pageFilterOptions} onChange={handleFilterChange}/>}/>
   </MainContainer>;
