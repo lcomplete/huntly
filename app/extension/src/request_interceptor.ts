645ef403df99fa2042cd575f41db25eb5fbd4344
lcomplete <louchenabc@gmail.com>
2025-09-03 18:02:24 +0800

    chore: update version number to 0.4.7.2 in manifest.json and clean up request interceptor implementation

--- app/extension/src/request_interceptor.ts
+++ app/extension/src/request_interceptor.ts
@@ -1,66 +1,32 @@
 const RawRequest = {
-  originalXHR: window.XMLHttpRequest,
-  originalFetch: window.fetch.bind(window),
+  originalOpen: XMLHttpRequest.prototype.open,
 }
-
-function customXHR(handler: (responseText: string, responseUrl: string) => void) {
-  return function() {
-    const xhr = new RawRequest.originalXHR;
-    for (let attr in xhr) {
-      if (attr === 'onreadystatechange') {
-        xhr.onreadystatechange = (...args) => {
-          if (this.readyState === 4 && (this.responseType === 'json' || this.responseType === 'text' || this.responseType === '')) {
-            handler(this.responseText, this.responseURL);
-          }
-          this.onreadystatechange && this.onreadystatechange.apply(this, args);
-        }
-        continue;
-      } else if (attr === 'onload') {
-        xhr.onload = (...args) => {
-          this.onload && this.onload.apply(this, args);
-        }
-        continue;
-      }
-
-      if (typeof xhr[attr] === 'function') {
-        this[attr] = xhr[attr].bind(xhr);
-      } else {
-        // responseText和response不是writeable的，但拦截时需要修改它，所以修改就存储在this[`_${attr}`]上
-        if (attr === 'responseText' || attr === 'response') {
-          Object.defineProperty(this, attr, {
-            get: () => this[`_${attr}`] === undefined ? xhr[attr] : this[`_${attr}`],
-            set: (val) => this[`_${attr}`] = val,
-            enumerable: true
-          });
-        } else {
-          Object.defineProperty(this, attr, {
-            get: () => xhr[attr],
-            set: (val) => xhr[attr] = val,
-            enumerable: true
-          });
-        }
-      }
-    }
-  }
-}
-
-// function myFetch(...args) {
-//   return RawRequest.originalFetch.apply(null, args).then((response) => {
-//     handleResponse(response.text, response.url)
-//     console.log({"fetchResponse": response});
-//     return response;
-//   });
-// }
 
 export class RequestInterceptor {
+
   enable(responseHandler: (responseText: string, responseUrl: string) => void) {
-    window.XMLHttpRequest = customXHR(responseHandler);
-    // window.fetch = myFetch;
+    // 重写 XMLHttpRequest.prototype.open 方法
+    XMLHttpRequest.prototype.open = function(method: string, url: string | URL, async?: boolean, user?: string | null, password?: string | null) {
+      // 添加 readystatechange 监听器
+      this.addEventListener('readystatechange', function() {
+        if (this.readyState === 4 && 
+            (this.responseType === 'json' || this.responseType === 'text' || this.responseType === '')) {
+          try {
+            responseHandler(this.responseText, this.responseURL);
+          } catch (error) {
+            console.warn('RequestInterceptor handler error:', error);
+          }
+        }
+      }, { once: true });
+
+      // 调用原始的 open 方法
+      return RawRequest.originalOpen.apply(this, arguments);
+    };
   }
 
   disable() {
-    window.XMLHttpRequest = RawRequest.originalXHR;
-    // window.fetch = RawRequest.originalFetch;
+    // 恢复原始的 open 方法
+    XMLHttpRequest.prototype.open = RawRequest.originalOpen;
   }
 }
 
