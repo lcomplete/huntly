a0d542003c12d9aacd713022dc3f37229c636f00
Yang Yang <i@gsalex.net>
2026-06-13 21:01:58 +0800

    refactor(server): rename OpenAPI config
    Rename the legacy DocketConfig class to OpenApiConfig after migrating from
    Springfox to springdoc. The OpenAPI bean behavior remains unchanged.

--- app/server/huntly-server/src/main/java/com/huntly/server/config/OpenApiConfig.java
+++ app/server/huntly-server/src/main/java/com/huntly/server/config/OpenApiConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class DocketConfig {
+public class OpenApiConfig {
     @Bean
     public OpenAPI huntlyOpenAPI() {
         return new OpenAPI()
